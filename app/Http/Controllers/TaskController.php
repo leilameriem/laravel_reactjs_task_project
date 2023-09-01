ed6c44ff3959a4425195b37ed0667649c6023bec
leilameriem <lielanuit345@gmail.com>
2023-09-01 01:11:06 +0100

    (Adding Routes & Fetching Tasks)

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -6,6 +6,8 @@ use Illuminate\Http\Request;
 use App\Models\Task;
 use App\Http\Requests\CreateTaskRequest;
 use App\Http\Requests\UpdateTaskRequest;
+use App\Models\Category;
+
 
 class TaskController extends Controller
 {
@@ -58,6 +60,25 @@ class TaskController extends Controller
    
     public function destroy(string $id)
     {
-        //
+        $task->delete();
+        return ['message'=>'Your task has been removed'];
     }
+    public function getTaskByCategory(Category $category){
+        return $category->tasks()->with('category')->paginate(10);
+    }
+    public function getTaskOrderBy($column, $direction){
+        return Task:with('category')->orderBy($column,$direction)->paginate(10);
+
+    }
+    public function getTaskByTerm($term){
+        $tasks=Task::with('category')
+        ->where('title','like','%'.$term.'%')
+        ->orwhere('body','like','%'.$term.'%')
+        ->orwhere('id','like','%'.$term.'%')
+        ->paginate(10);
+
+        return $tasks;
+
+    }
+
 }
--- app/Models/Task.php
+++ app/Models/Task.php
@@ -4,8 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Category;
 
 class Task extends Model
 {
     use HasFactory;
+
+    protected $fillable=['title','body','done','category_id'];
+
+    public function category(){
+    	return $this->belongsTo(Category::class);
+
+    }
+
 }
